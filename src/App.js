f0de04aab64327d802bee2d4ba332bdd7a695c5c
keyur diwan <keyurdiwan009@gmail.com>
2022-03-22 14:33:09 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,31 +1,84 @@
-import { Route, Routes, useNavigate } from 'react-router-dom';
-import './App.css';
-import Cart from './Components/Cart';
-import Navbar from './Components/Navbar';
-import Product from './Components/Product';
+// // import { Route, Routes, useNavigate } from 'react-router-dom';
+// // import './App.css';
+// // import Cart from './Components/Cart';
+// // import Navbar from './Components/Navbar';
+// // import Product from './Components/Product';
+
+// import "./App.css";
+// import {Header} from "./components/Header";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import Home from "./components/Home";
+// import Cart from "./components/Cart";
 
 
-// json-server db.json --port 3001 --watch
-function App() {
-  return (
-    <>
+// /*
+
+   
     
-
-       <Navbar />
-    
-      <Routes>
+//       <Routes>
        
-        <Route path="/" element={<Product/>}></Route>
+//         <Route path="/" element={<Product/>}></Route>
       
 
-        <Route path="/cart/:id" element={<Cart />}></Route>
+//         <Route path="/cart/:id" element={<Cart />}></Route>
       
        
    
 
-      </Routes>
-    </>
+//       </Routes>
+//  */
+
+// // json-server db.json --port 3001 --watch
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Header />
+//       <div className="App">
+//         {/* <Routes>
+         
+//            <Route path="/" element={ <Home />} ></Route>
+         
+     
+
+//        <Route path="/cart" element = { <Cart />}>
+         
+//         </Route>
+//          </Routes> */}
+   
+       
+//       </div>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
+
+import "./App.css";
+import Header from "./components/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
+
+function App() {
+  return (
+   <>
+      <Header />
+       
+      <div className="App">
+         <Routes>
+        <Route path="/" element = {<Home/>}>
+        
+        </Route>
+        <Route path="/cart" element = {  <Cart />}>
+        
+          </Route>
+          </Routes>
+      </div>
+      </>
+   
   );
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Context from "./context/Context";
+import "bootstrap/dist/css/bootstrap.min.css";
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Context>
   <BrowserRouter>
         <App />
-    </BrowserRouter>
+      </BrowserRouter>
+      </Context>
   </React.StrictMode>,
   document.getElementById('root')
 );
